a03cdabfb7af4f6fdd7026227971eaa3d988bb3c
Nareg Khoshafian <nareg@intrepid.io>
2016-03-25 09:37:08 -0400

    Stable animation. 1

--- DynamicsDemo/LogInViewController.swift
+++ DynamicsDemo/LogInViewController.swift
@@ -27,21 +27,24 @@ class CustomPresentAnimationController: NSObject, UIViewControllerAnimatedTransi
     func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
         return 2.5
     }
-    // 2. This required method uses the transitionContext to get the view controller we are navigating
-    // from(LogInViewController, to (UserProfileViewController), the final frame the transition contaxt should have after the animation completes and the container view which houses the views corrosponding to the, to and from view controllers
+    // 2. This required method uses the transitionContext to get the view controller we are navigating from(LogInViewController), to (UserProfileViewController), the final frame the transition contaxt should have after the animation completes and the container view which houses the views corrosponding to the, to and from view controllers
     func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
         
         let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
         let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
         let finalFrameForVC = transitionContext.finalFrameForViewController(toViewController)
+        // This holds the to/from controllers
         let containerView = transitionContext.containerView()
+        containerView?.backgroundColor = UIColor.blueColor()
+        // We position the toView just below the bottom of the screen.
         let bounds = UIScreen.mainScreen().bounds
-        toViewController.view.frame = CGRectOffset(finalFrameForVC, 0, bounds.size.height)
+        toViewController.view.frame = CGRectOffset(finalFrameForVC, 0, bounds.size.height - 50)
         containerView!.addSubview(toViewController.view)
         
-        UIView.animateWithDuration(transitionDuration(transitionContext), delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: .CurveLinear, animations: {
-            fromViewController.view.alpha = 0.5
-            toViewController.view.frame = CGRect(x: 0, y: 50, width: bounds.size.width, height: bounds.size.height)
+        // The transition used here is the same as the one defined above.
+        UIView.animateWithDuration(transitionDuration(transitionContext), delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.0, options: .CurveLinear, animations: {
+            fromViewController.view.alpha = 0.0
+            toViewController.view.frame = CGRect(x: 0, y: 50, width: bounds.size.width, height: bounds.size.height + 200 )
             }, completion: {
                 finished in
                 transitionContext.completeTransition(true)
